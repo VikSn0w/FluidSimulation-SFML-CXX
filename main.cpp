c8d99286933b072ba9585553059933fe59fb6d73
viksn0w <vittoriopicone30@gmail.com>
2023-10-21 00:58:35 +0200

    1. Better collision detection with control on particle oscillation when approaching near-zero velocity 2. Code optimization

--- main.cpp
+++ main.cpp
@@ -11,7 +11,8 @@
 #define PARTICLE_RADIUS 10.f
 #define GRAVITATIONAL_COSTANT 9.81f
 #define PARTICLE_MASS 100
-#define ELASTIC_COEFFICIENT 0.7
+#define ELASTIC_COEFFICIENT 0.5
+#define VELOCITY_THRESHOLD 0.01f
 
 int Sign(double number){
     if (number > 0) {
@@ -23,12 +24,7 @@ int Sign(double number){
     }
 }
 
-struct Position {
-    double x = 0.f;
-    double y = 0.f;
-};
-
-struct Velocity {
+struct Vector2 {
     double x = 0.f;
     double y = 0.f;
 };
@@ -38,20 +34,30 @@ struct Particle {
     sf::RenderTexture renderTexture;
     sf::Sprite sprite;
     sf::Shader blurShader;
-    Position position;
-    Velocity velocity;
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 screen;
+    Vector2 position_start;
     double elastic_coefficient = 0.f;
     double mass = 0.f;
+    double diameter = 0.f;
+    double radius = 0.f;
 
     // Constructor to initialize the particle
     Particle(double r, double x, double y, double ec, double m, double screen_x, double screen_y) : circle(r) {
         // Customize the particle's properties here if needed
         circle.setFillColor(sf::Color(14,135,204));
         circle.setPosition(x, y);
+        position_start.x = x;
+        position_start.y = y;
         elastic_coefficient = ec;
         position.x = x;
         position.y = y;
         mass = m;
+        diameter = r*2;
+        radius = r;
+        screen.x = screen_x;
+        screen.y = screen_y;
 
         renderTexture.create(screen_x,screen_y);
         renderTexture.clear(sf::Color::Transparent);
@@ -80,36 +86,24 @@ struct Particle {
         position.x = x;
     }
 
+    void updateParticle(double deltaTime){
+        velocity.y += (-GRAVITATIONAL_COSTANT*mass) * -1 * deltaTime;
+        position.y += velocity.y * deltaTime;
+        setPosition(circle.getPosition().x, position.y);
+    }
+
+    void checkForCollisions(){
+        if(std::abs(circle.getPosition().x) + position_start.x > SCREEN_SIZE_X - (diameter) && std::abs(velocity.x) > VELOCITY_THRESHOLD){
+            setPosition((SCREEN_SIZE_X - position_start.x) - diameter * Sign(circle.getPosition().x), position.y);
+            velocity.x *= -1 * elastic_coefficient;
+        }
+        if(std::abs(circle.getPosition().y) + position_start.y> SCREEN_SIZE_Y - (diameter) && std::abs(velocity.y) > VELOCITY_THRESHOLD){
+            setPosition(position.x, (SCREEN_SIZE_Y - position_start.y) - diameter * Sign(circle.getPosition().y));
+            velocity.y *= -1 * elastic_coefficient;
+        }
+    }
 };
 
-void updateParticle(Position &position, Velocity &velocity, double deltaTime, Particle &particle){
-    velocity.y += (-GRAVITATIONAL_COSTANT*particle.mass) * -1 * deltaTime;
-    position.y += velocity.y * deltaTime;
-    particle.setPosition(particle.circle.getPosition().x, position.y);
-    printf("y: %f\n", position.y);
-};
-
-void foo(Particle &particle,double x, double y){
-    particle.setPosition(x,y);
-    particle.position.y = y;
-    particle.position.x = x;
-}
-
-void checkForCollisions(Particle &particle){
-    double d = particle.circle.getRadius()*2;
-    if(abs(particle.circle.getPosition().x) > SCREEN_SIZE_X - d){
-        float x = SCREEN_SIZE_X*d * Sign(particle.circle.getPosition().x);
-        particle.position.x = x;
-        particle.circle.setPosition(x, particle.circle.getPosition().y);
-        particle.velocity.x *= -1 * particle.elastic_coefficient;
-    }
-    if(abs(particle.circle.getPosition().y) > SCREEN_SIZE_Y - d){
-        float y = SCREEN_SIZE_Y - d * Sign(particle.circle.getPosition().y);
-        particle.position.y = y;
-        particle.circle.setPosition(particle.circle.getPosition().x, y);
-        particle.velocity.y *= -1 * particle.elastic_coefficient;
-    }
-}
 
 
 int main() {
@@ -119,24 +113,28 @@ int main() {
     window.setFramerateLimit(FPS_LIMIT);
     sf::Clock frameClock;
 
-    Particle particle(PARTICLE_RADIUS, 0,10, ELASTIC_COEFFICIENT, PARTICLE_MASS, window.getSize().x, window.getSize().y);
-
-    Velocity velocity;
-    Position position;
-
+    Particle particles[2] = {
+            {PARTICLE_RADIUS, 10,40, ELASTIC_COEFFICIENT, PARTICLE_MASS, static_cast<double>(window.getSize().x), static_cast<double>(window.getSize().y)},
+            {PARTICLE_RADIUS, 120,50, ELASTIC_COEFFICIENT, PARTICLE_MASS, static_cast<double>(window.getSize().x), static_cast<double>(window.getSize().y)}
+    };
     while (window.isOpen()) {
         sf::Time frameTime = frameClock.restart(); // Get the time elapsed since the last frame
         float deltaTime = frameTime.asSeconds();
 
-        updateParticle(particle.position, particle.velocity, deltaTime, particle);
 
         window.clear(sf::Color::Black);
 
-        velocity.y += (-GRAVITATIONAL_COSTANT*particle.mass) * -1 * deltaTime;
-        position.y += velocity.y * deltaTime;
-        foo(particle, 0, position.y);
-        printf("x: %f",particle.position.x);
-        window.draw(particle.sprite, &particle.blurShader);
+        for (int i = 0; i < 2; ++i) {
+            particles[i].updateParticle(deltaTime);
+            particles[i].checkForCollisions();
+            printf("P%d y: %f ",i,particles[i].position.y);
+            printf("P%d y: %f ",i,abs(particles[i].circle.getPosition().y) + particles[i].position_start.y + particles[i].diameter);
+            //printf("X: %u",particle.renderTexture.getTexture().getSize().x);
+
+            window.draw(particles[i].sprite, &particles[i].blurShader);
+        }
+        printf("\n");
+
         window.display();
 
         sf::Event event;
